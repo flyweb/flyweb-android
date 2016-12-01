5a3eb4348fd26dd1915de8f4d40ebe683493387c
Kannan Vijayan <kvijayan@mozilla.com>
2016-11-30 22:43:19 -0500

    Added file upload support.

--- app/src/main/java/ca/vijayan/flyweb/BrowseActivity.java
+++ app/src/main/java/ca/vijayan/flyweb/BrowseActivity.java
@@ -2,10 +2,13 @@ package ca.vijayan.flyweb;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -20,11 +23,13 @@ public class BrowseActivity extends Activity {
     DNSServiceInfo mServiceInfo;
     WebView mWebView;
     CookieManager mCookieManager;
+    ValueCallback<Uri[]> mFilePathCallback;
 
     static final String MOBILE_UA =
         "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) " +
                 "AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1\n";
 
+    private static final int FILE_CHOOSER_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +46,35 @@ public class BrowseActivity extends Activity {
         mWebView = (WebView) group.findViewById(R.id.browse_webview);
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
-        settings.setUserAgentString(MOBILE_UA);
         settings.setSupportZoom(true);
         settings.setBuiltInZoomControls(true);
         settings.setDisplayZoomControls(false);
         mWebView.setWebViewClient(new WebViewClient() {
         });
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                Intent intent = fileChooserParams.createIntent();
+                mFilePathCallback = filePathCallback;
+                startActivityForResult(intent, FILE_CHOOSER_REQUEST_CODE);
+                return true;
+            }
+        });
         mCookieManager = CookieManager.getInstance();
         mCookieManager.setAcceptCookie(true);
         mCookieManager.setAcceptThirdPartyCookies(mWebView, false);
         mWebView.loadUrl(mServiceInfo.getServiceURL());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == FILE_CHOOSER_REQUEST_CODE) {
+            mFilePathCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+            mFilePathCallback = null;
+        }
     }
 
     @Override
